eedb1c68963170e636ffaa73f3a9e64cd395760b
dylancvb <dcvanbramer@gmail.com>
2023-07-11 16:32:52 -0400

    Streamlined neighborhood data structure
    - Next: k folds, data processing file

--- data_processing.py
+++ data_processing.py
@@ -29,6 +29,9 @@ def clean_t1(ds):
     # keep only IDs, species, xy coords, and dbh
     ds = ds[['treeID', 'sp', 'gx', 'gy', 'dbh']]
     ds = ds.rename(columns={"dbh": "dbh1", "treeID": "treeID1"})
+    return ds
+
+    print("t1 is"+str(type(ds)))
 
 
 def clean_t2(ds):
@@ -36,26 +39,37 @@ def clean_t2(ds):
     expected_labels = ds[['treeID', 'dbh']]
     expected_labels = expected_labels.rename(
         columns={"dbh": "dbh2", "treeID": "treeID2"})
+    return ds
+    print("t2 is"+str(type(ds)))
 
 
-def encode_drop_null(t1, t2):
+def encode_drop_null(t1: pd.DataFrame, t2: pd.DataFrame):
     """
     returns numpy arrays of the two times - (t1, t2)
     """
-    t1 = clean_t1(t1)
-    t2 = clean_t2(t2)
+
+    t1_clean = clean_t1(t1)
+    t2_clean = clean_t2(t2)
+    # print("t1 is"+str(type(t1_clean)))
+    # print("t2 is"+str(type(t2_clean)))
 
     # encode the species into categorical data rather than strings
     encoder = ce.BinaryEncoder(cols=['sp'], return_df=True)
-    t1 = encoder.fit_transform(t1)
-    ds_combined = pd.concat([t1, t2], axis=1)
+
+    t1_clean = encoder.fit_transform(t1_clean)
+    ds_combined = pd.concat([t1_clean, t2_clean], axis=1)
+    print(type(ds_combined))
 
     # drop rows with any NaN values in either t1 or t2 dbhs
-    ds_combined_clean = ds_combined.dropna()
+    ds_combined_clean = pd.DataFrame.dropna(ds_combined)
+
+    print("ds combined clean is" + str(ds_combined_clean))
 
     # convert these non-null datasets into numpy arrays and return
-    t1_updated = ds_combined_clean[t1.columns].to_numpy()
-    t2_updated = ds_combined_clean[t2.columns].to_numpy()
+    t1_updated = ds_combined_clean[t1_clean.columns].to_numpy()
+    t2_updated = ds_combined_clean[t2_clean.columns].to_numpy()
+    # print(t1_updated)
+    # print(t2_updated)
     return (t1_updated, t2_updated)
 
 
@@ -66,7 +80,9 @@ def growth_labels(t1, t2):
 
 
 def split(t1, t2, split_prop):
-    X_train, X_test, y_train, y_test = train_test_split(t1, t2, test_size=0.3)
+    print(t1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        t1, t2, test_size=split_prop)
     feats = X_train[:, 1:]
     labels = y_train[:, 1]
 
@@ -100,7 +116,9 @@ def clean(file_paths, num_quads=4924, split_prop=0.3):
        training)
     """
     data_t1 = _load_data(file_paths[0], num_quads)
+    print("data-t1 is"+str(type(data_t1)))
     data_t2 = _load_data(file_paths[1], num_quads)
+    print("data-t2 is"+str(type(data_t1)))
     data_t1, data_t2 = encode_drop_null(data_t1, data_t2)
     data_t2 = growth_labels(data_t1, data_t2)
     return split(data_t1, data_t2, split_prop)
